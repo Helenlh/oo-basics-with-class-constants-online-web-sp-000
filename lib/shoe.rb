cac26334eb0079c3d42ac245599fb299c78cb760
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-29 00:53:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,10 @@ class Shoe
   
   def initialize(brand)
     @brand= brand 
-    !BRANDS.include? brand 
+    if !BRANDS.include? brand 
      BRANDS << brand 
    end 
+ end 
 
   def size
     @size= 3
